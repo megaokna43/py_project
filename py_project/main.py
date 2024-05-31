8e5da95bf2274379463c8d04a6834ed20d38c777
megaokna43 <fitobar43@gmail.com>
2024-05-31 22:57:10 +0300

    дополнилй функцией сложения  файл main.py

--- py_project/main.py
+++ py_project/main.py
@@ -2,8 +2,8 @@
 
 num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
-result1 = num1 + num2
-print(f"Сумма чисел: {result1}")
+result_1 = num1 + num2
+print(f"Сумма чисел: {result_1}")
 
 #вычитаем введеных 2 числа
 print('сейчас рассчитаем разность')
