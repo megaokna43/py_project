e8daeb965b376bab73eb88ff746457f57589aeac
megaokna43 <fitobar43@gmail.com>
2024-05-31 22:48:48 +0300

    создал файл main.py

--- py_project/fyncs.py
+++ py_project/fyncs.py
@@ -1,11 +1,4 @@
-#складываем 2 числа
-
-num1 = float(input("Введите первое число: "))
-num2 = float(input("Введите второе число: "))
-result1 = num1 + num2
-print(f"Сумма чисел: {result1}")
-
+#вычитаем 2 числа
 print('сейчас рассчитаем разность')
-
-result2 = num1 - num2
-print(f"Сумма чисел: {result2}")
+result_2 = num1 - num2
+print(f"Сумма чисел: {result_2}")
---
+++ py_project/main.py
@@ -0,0 +1,6 @@
+#складываем 2 числа
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+result1 = num1 + num2
+print(f"Сумма чисел: {resulgitt1}")
