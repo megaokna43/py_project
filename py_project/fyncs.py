bcb70d75a38ae5373e1908c871c264ba143e75a7
megaokna43 <fitobar43@gmail.com>
2024-05-31 22:39:38 +0300

    дополнил файл funcs.py

---
+++ py_project/fyncs.py
@@ -0,0 +1,11 @@
+#складываем 2 числа
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+result1 = num1 + num2
+print(f"Сумма чисел: {result1}")
+
+print('сейчас рассчитаем разность')
+
+result2 = num1 - num2
+print(f"Сумма чисел: {result2}")
