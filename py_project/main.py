db9501898e5f98f7dcfc56eed47ac5e244eed1e8
megaokna43 <fitobar43@gmail.com>
2024-05-31 22:58:31 +0300

    дополнилй функцией вычитания файл main.py

--- py_project/main.py
+++ py_project/main.py
@@ -7,5 +7,5 @@ print(f"Сумма чисел: {result_1}")
 
 #вычитаем введеных 2 числа
 print('сейчас рассчитаем разность')
-result_2 = num1 - num2
-print(f"Сумма чисел: {result_2}")
+result2 = num1 - num2
+print(f"Сумма чисел: {result2}")
